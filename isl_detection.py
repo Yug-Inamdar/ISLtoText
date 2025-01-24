a8dab9055bc55d261d9ef40025034b8d9fa12e8f
Yug-Inamdar <Yuginamdar04@gmail.com>
2025-01-24 18:39:58 +0530

    Initial commit

--- isl_detection.py
+++ isl_detection.py
@@ -22,7 +22,7 @@ def calc_landmark_list(image, landmarks):
 
     landmark_point = []
 
-    # Keypoint
+    # Keypointcopilot
     for _, landmark in enumerate(landmarks.landmark):
         landmark_x = min(int(landmark.x * image_width), image_width - 1)
         landmark_y = min(int(landmark.y * image_height), image_height - 1)
